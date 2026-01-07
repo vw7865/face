b7e1dd8be07f7fb96f03428d604fc5349dc2473f
vw7865 <47992851+vw7865@users.noreply.github.com>
2026-01-07 15:55:33 -0700

    Initial Commit

---
+++ LooksmaxAI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LooksmaxAI
+//
+//  Created by Admin on 2026. 01. 07..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LooksmaxAITests/LooksmaxAITests.swift
@@ -0,0 +1,17 @@
+//
+//  LooksmaxAITests.swift
+//  LooksmaxAITests
+//
+//  Created by Admin on 2026. 01. 07..
+//
+
+import Testing
+@testable import LooksmaxAI
+
+struct LooksmaxAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
